d140ff813e59d77a186dca83f1f829304988b4d2
HieuAnh <dhieu23nd@gmail.com>
2023-02-22 11:26:34 +0700

    Add: login feature

--- userauths/urls.py
+++ userauths/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from userauths.views import register
+from userauths.views import login_view, register_view
 
 app_name = "userauths"
 
 urlpatterns = [
-    path("sign-up/", register, name="sign-up"),
+    path("sign-up/", register_view, name="sign-up"),
+    path("sign-in/", login_view, name="sign-in"),
 ]
--- userauths/views.py
+++ userauths/views.py
@@ -1,12 +1,14 @@
+from django.contrib import messages
 from django.shortcuts import render, redirect
 from userauths.forms import UserRegisterForm
 from django.contrib.auth import authenticate, login, logout
-from django.contrib import messages
+from .models import User
 
 
-def register(request):
+def register_view(request):
     if request.method == "POST":
         form = UserRegisterForm(request.POST or None)
+        # Check if the form is valid
         if form.is_valid():
             new_user = form.save()
             username = form.cleaned_data.get('username')
@@ -19,8 +21,34 @@ def register(request):
     else:
         print("User can not register")
         form = UserRegisterForm()
-
     context = {
         "form": form,
     }
     return render(request, "userauths/sign-up.html", context)
+
+
+def login_view(request):
+    # Check if the user is already logged in
+    if request.user.is_authenticated:
+        messages.warning(request, "You are already logged in")
+        return redirect("core:index")
+    if request.method == "POST":
+        email = request.POST.get("email")
+        password = request.POST.get("password")
+        # Check if the user exists
+        try:
+            user = User.objects.get(email=email)
+        except User.DoesNotExist:
+            messages.warning(request, f"User with email {email} does not exist")
+            return redirect("userauths:login")
+        # Check if the user is authenticated
+        user = authenticate(request, username=email, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, f"Welcome back {user.username}")
+            return redirect("core:index")
+        else:
+            messages.warning(request, "Email or password is incorrect")
+    context = {}
+    return render(request, "userauths/sign-in.html", context)
+
