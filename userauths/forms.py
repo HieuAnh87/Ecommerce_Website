52dc416a3ee8c16ebb4d7652aa7cbc02a6e4c653
HieuAnh <dhieu23nd@gmail.com>
2023-02-22 10:48:30 +0700

    Add: backend sign-up
    form validate + xử lí request

--- userauths/forms.py
+++ userauths/forms.py
@@ -5,6 +5,11 @@ from userauths.models import User
 
 class UserRegisterForm(UserCreationForm):
     # Use this UserCreationForm to create a new user auth, access it from the admin panel
+    username = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Username'}))
+    email = forms.EmailField(widget=forms.TextInput(attrs={'placeholder': 'Email'}))
+    password1 = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Password'}))
+    password2 = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Confirm Password'}))
+
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
--- userauths/views.py
+++ userauths/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from userauths.forms import UserRegisterForm
 from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 
 def register(request):
@@ -9,8 +10,12 @@ def register(request):
         if form.is_valid():
             new_user = form.save()
             username = form.cleaned_data.get('username')
-
+            messages.success(request, f"Account created for {username}")
+            new_user = authenticate(username=form.cleaned_data['email'],
+                                    password=form.cleaned_data['password1'])
+            login(request, new_user)
             print("User registered")
+            return redirect("core:index")
     else:
         print("User can not register")
         form = UserRegisterForm()
