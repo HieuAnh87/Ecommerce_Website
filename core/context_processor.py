27874963eeb926c95ea5845813614241fd9a7110
HieuAnh <dhieu23nd@gmail.com>
2023-03-01 10:21:36 +0700

    Add: address into default context

--- core/context_processor.py
+++ core/context_processor.py
@@ -1,8 +1,10 @@
-from core.models import Category
+from core.models import Category, Address
 
 
 def default(request):
     categories = Category.objects.all()
+    address = Address.objects.filter(user=request.user)
     return {
         'categories': categories,
+        'address': address
     }
